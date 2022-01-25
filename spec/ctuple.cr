ba92c74f18af88cf2dd4d19f2a03ada6fb0a6b90
Andy Baird <andybaird@gmail.com>
2022-01-25 00:00:44 -0700

    Chapter 1 tests: creating, adding, subtracting

---
+++ spec/ctuple.cr
@@ -0,0 +1,60 @@
+require "./spec_helper.cr"
+
+describe CTuple do
+  describe "#new" do
+    it "initializes with xyzw parameters" do
+      CTuple.new(1.0, -2.0, 3.0, 1.0).val.should eq({1.0, -2.0, 3.0, 1.0 })  
+    end
+    it "initializes with a factory method for points" do
+      point = CTuple.new_point(1.0,1.0,1.0)
+      point.w.should eq 1.0
+      point.point?.should eq true
+    end
+    it "initializes with a factory method for vectors" do
+      vector = CTuple.new_vector(1.0,1.0,1.0) 
+      vector.w.should eq 0.0
+      vector.vector?.should eq true
+    end
+  end
+
+  describe "#math" do
+    it "a point added to a vector creates a new point" do
+      point = CTuple.new(3, -2, 5, 1)
+      vector = CTuple.new(-2, 3, 1, 0)
+
+      result = point + vector
+
+      result.val.should eq({1.0, 1.0, 6.0, 1.0})
+      result.point?.should eq true
+    end
+
+    it "subtracting two points creates a vector" do
+      p1 = CTuple.new_point(3, 2, 1)
+      p2 = CTuple.new_point(5, 6, 7)
+
+      result = p1 - p2
+
+      result.val.should eq({-2.0, -4.0, -6.0, 0.0})
+      result.vector?.should eq true
+    end
+
+    it "subtracting a vector from a point creates a point" do
+      point = CTuple.new_point(3,2,1)
+      vector = CTuple.new_vector(5,6,7)
+
+      result = point - vector
+      result.val.should eq({-2.0,-4.0,-6.0,1.0})
+      result.point?.should eq true
+    end
+
+    it "subtracting two vectors creates a vector" do
+      v1 = CTuple.new_vector(3,2,1)
+      v2 = CTuple.new_vector(5,6,7)
+
+      result = v1 - v2
+      result.val.should eq({-2.0,-4.0,-6.0,0.0})
+      result.vector?.should eq true
+    end
+  end
+  
+end
---
+++ src/ctuple.cr
@@ -0,0 +1,59 @@
+
+# CTuple (Coordinate Tuple)
+#
+# This class stores a tuple value representation of a point or vector in a 3D space
+# and provides utility methods for creating and accessing coordinatae values.
+
+class CTuple
+  def initialize(tuple)
+    @tuple = tuple
+  end
+
+  def initialize(x : Float64, y : Float64, z : Float64, w : Float64)
+    @tuple = {x,y,z,w}
+  end
+
+  def x
+    @tuple[0]
+  end
+
+  def y
+    @tuple[1]
+  end
+
+  def z
+    @tuple[2]
+  end
+
+  def w
+    @tuple[3]
+  end
+
+  def val
+    @tuple
+  end
+
+  def point?
+    w == 1.0
+  end
+
+  def vector?
+    w == 0.0
+  end
+
+  def self.new_point(x : Float64, y : Float64, z : Float64)
+    CTuple.new(x, y, z, 1.0)
+  end
+
+  def self.new_vector(x : Float64, y : Float64, z : Float64)
+    CTuple.new(x, y, z, 0.0)
+  end
+
+  def +(t : CTuple)
+    CTuple.new(x + t.x, y + t.y, z + t.z, w + t.w)
+  end
+
+  def -(t : CTuple)
+    CTuple.new(x - t.x, y - t.y, z - t.z, w - t.w)
+  end
+end
