9a39270f66d6b53e5bb7438900f1a463285c48d6
Andy Baird <andybaird@gmail.com>
2022-02-18 16:10:51 -0700

    Started matrix class with initialization tests

--- spec/matrix_spec.cr
+++ spec/matrix_spec.cr
@@ -3,7 +3,20 @@ require "./spec_helper.cr"
 describe Matrix do
   describe "#new" do
     it "initializes with a 4x4 matrix" do
-      
+      m = Matrix.new([
+        [ 1,    2,    3,    4],
+        [ 5.5,  6.5,  7.5,  8.5],
+        [ 9,    10,   11,   12 ],
+        [ 13.5, 14.5, 15.5, 16.5]
+      ])
+
+      m[0][0].should eq 1
+      m[0][3].should eq 4
+      m[1][0].should eq 5.5
+      m[1][2].should eq 7.5
+      m[2][2].should eq 11
+      m[3][0].should eq 13.5
+      m[3][2].should eq 15.5
     end
   end
 end
--- src/raytracer/matrix.cr
+++ src/raytracer/matrix.cr
@@ -1,11 +1,21 @@
 class Matrix
-  getter rows
-  getter columns
-  getter matrix
+  getter rows : Int32
+  getter columns : Int32
+  getter matrix : Array(Array(Float64) | Array(Int32))
 
   def initialize(rows, columns)
     @rows = rows
     @columns = columns
-    @matrix = Array.new(rows) { Array.new(columns) {  }}
+    @matrix = Array.new(rows) { Array.new(columns) { 0 }}
+  end
+
+  def initialize(matrix : Array(Array(Float64) | Array(Int32)))
+    @rows = matrix.size
+    @columns = matrix[0].size
+    @matrix = matrix
+  end
+
+  def [](row : Int)
+    @matrix[row]
   end
 end
