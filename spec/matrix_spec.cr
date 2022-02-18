6683b36b4b05ea6e769c7d45e2f9aa03f5254fab
Andy Baird <andybaird@gmail.com>
2022-02-18 16:39:28 -0700

    Started matrix math implementation (multiplication)

--- spec/matrix_spec.cr
+++ spec/matrix_spec.cr
@@ -4,9 +4,9 @@ describe Matrix do
   describe "#new" do
     it "initializes with a 4x4 matrix" do
       m = Matrix.new([
-        [ 1,    2,    3,    4],
+        [ 1.0,  2.0,  3.0,  4.0],
         [ 5.5,  6.5,  7.5,  8.5],
-        [ 9,    10,   11,   12 ],
+        [ 9.0,  10.0, 11.0, 12.0 ],
         [ 13.5, 14.5, 15.5, 16.5]
       ])
 
@@ -17,6 +17,98 @@ describe Matrix do
       m[2][2].should eq 11
       m[3][0].should eq 13.5
       m[3][2].should eq 15.5
+    end
+    it "initializes with a 2x2 matrix" do
+      m = Matrix.new([
+        [-3.0, 5.0],
+        [ 1.0,-2.0]
+      ])
+
+      m[0][0].should eq -3
+      m[0][1].should eq 5
+      m[1][0].should eq 1
+      m[1][1].should eq -2
+    end
+    it "initializes with a 3x3 matrix" do
+      m = Matrix.new([
+        [ -3.0,  5.0,  0.0 ],
+        [  1.0, -2.0, -7.0 ],
+        [  0.0,  1.0,  1.0 ]
+      ])
+
+      m[0][0].should eq -3
+      m[1][1].should eq -2
+      m[2][2].should eq 1
+    end
+    it "casts integers to float64" do
+      m = Matrix.new([
+        [ -3,  5,  0 ],
+        [  1, -2, -7 ],
+        [  0,  1,  1 ]
+      ])
+
+      m[0][0].should eq -3
+      m[1][1].should eq -2
+      m[2][2].should eq 1
+    end
+  end
+  describe "#equality" do
+    it "recognizes matrices with the same values as equal" do
+      a = Matrix.new([
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,8,7,6],
+        [5,4,3,2]
+      ])
+      b = Matrix.new([
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,8,7,6],
+        [5,4,3,2]
+      ])
+
+      a.should eq b
+    end
+  end
+  describe "#math" do
+    it "multiples matrices together properly" do
+      # Scenario: Multiplying two matrices 
+      # Given the following matrix A:
+      #   |1|2|3|4| 
+      #   |5|6|7|8| 
+      #   |9|8|7|6| 
+      #   |5|4|3|2|
+      #   And the following matrix B: 
+      #   |-2|1|2| 3|
+      #   | 3|2|1|-1|
+      #   | 4|3|6| 5|
+      #   | 1|2|7| 8|
+      #   Then A * B is the following 4x4 matrix:
+      #   |20| 22| 50| 48| 
+      #   |44| 54|114|108| 
+      #   |40| 58|110|102| 
+      #   |16| 26| 46| 42|
+
+      a = Matrix.new([
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 8, 7, 6],
+        [5, 4, 3, 2]
+      ])
+
+      b = Matrix.new([
+        [-2, 1, 2, 3],
+        [ 3, 2, 1,-1],
+        [ 4, 3, 6, 5],
+        [ 1, 2, 7, 8]
+      ])
+
+      (a * b).should eq Matrix.new([
+        [ 20, 22,  50,  48],
+        [ 44, 54, 114, 108],
+        [ 40, 58, 110, 102],
+        [ 16, 26,  46,  42]
+      ])
     end
   end
 end
--- src/raytracer/matrix.cr
+++ src/raytracer/matrix.cr
@@ -1,7 +1,7 @@
 class Matrix
   getter rows : Int32
   getter columns : Int32
-  getter matrix : Array(Array(Float64) | Array(Int32))
+  getter matrix : Array(Array(Float64))
 
   def initialize(rows, columns)
     @rows = rows
@@ -9,10 +9,30 @@ class Matrix
     @matrix = Array.new(rows) { Array.new(columns) { 0 }}
   end
 
-  def initialize(matrix : Array(Array(Float64) | Array(Int32)))
+  def initialize(matrix : Array(Array(Float64)))
     @rows = matrix.size
     @columns = matrix[0].size
     @matrix = matrix
+  end
+
+  def initialize(matrix : Array(Array(Float64 | Int32)))
+    @rows = matrix.size
+    @columns = matrix[0].size
+    @matrix = Array.new(rows) { Array.new(columns) { 0.0 }}
+
+    @rows.times do |x|
+      @columns.times do |y|
+        @matrix[x][y] = matrix[x][y].to_f
+      end
+    end
+  end
+
+  def *(other : Matrix)
+    other
+  end
+
+  def ==(other : Matrix)
+    @matrix == other.matrix
   end
 
   def [](row : Int)
