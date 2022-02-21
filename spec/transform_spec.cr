33eba500a772b35760c15bd5753475cc247d845c
Andy Baird <andy@threadsculture.com>
2022-02-21 14:27:36 -0700

    Transform scaling matrices

--- spec/transform_spec.cr
+++ spec/transform_spec.cr
@@ -3,20 +3,47 @@ require "./spec_helper.cr"
 describe Transform do
   describe "transformations" do
     it "multiplies by a translation matrix" do
-      transform = Transform.new(5, -3, 2)
+      transform = Transform.translation(5, -3, 2)
       p = Point.new(-3, 4, 5)
       (transform * p).should eq Point.new(2,1,7)
     end
     it "multiplies by the inverse of a translation matrix" do
-      transform = Transform.new(5, -3, 2)
+      transform = Transform.translation(5, -3, 2)
       inv = transform.inverse
       p = Point.new(-3, 4, 5)
       (inv * p).should eq Point.new(-8, 7, 3)
     end
     it "is not affected by vectors" do
-      transform = Transform.new(5, -3, 2)
+      transform = Transform.translation(5, -3, 2)
       v = Vector.new(-3, 4, 5)
       (transform * v).should eq v
+    end
+  end
+  describe "scaling" do
+    it "matrix applied to a point" do
+      transform = Transform.scaling(2, 3, 4)
+      p = Point.new(-4, 6, 8)
+
+      (transform * p).should eq Point.new(-8, 18, 32)
+    end
+    it "matrix applied to a vector" do
+      transform = Transform.scaling(2, 3, 4)
+      v = Vector.new(-4, 6, 8)
+
+      (transform * v).should eq Vector.new(-8, 18, 32)
+    end
+    it "multiplies by the inverse of a scaling matrix" do
+      transform = Transform.scaling(2, 3, 4)
+      inv = transform.inverse
+      v = Vector.new(-4, 6, 8)
+
+      (inv * v).should eq Vector.new(-2, 2, 2)
+    end
+    it "reflection is scaling by a negative value" do
+      transform = Transform.scaling(-1, 1, 1)
+      p = Point.new(2, 3, 4)
+
+      (transform * p).should eq Point.new(-2, 3, 4)
     end
   end
 end
--- src/raytracer/transform.cr
+++ src/raytracer/transform.cr
@@ -1,11 +1,23 @@
 class Transform < Matrix
-  def initialize(x, y, z)
-    @matrix = Matrix.identity
-    @rows = 4
-    @columns = 4
 
-    @matrix[0][3] = x
-    @matrix[1][3] = y
-    @matrix[2][3] = z
+  def self.translation(x, y, z)
+    t = Matrix.new(Matrix.identity)
+
+    t.matrix[0][3] = x
+    t.matrix[1][3] = y
+    t.matrix[2][3] = z
+
+    t
+  end
+
+  def self.scaling(x, y, z)
+    t = Matrix.new(Matrix.identity)
+
+    t.matrix[0][0] = x
+    t.matrix[1][1] = y
+    t.matrix[2][2] = z
+    t.matrix[3][3] = 1.0
+
+    t
   end
 end
