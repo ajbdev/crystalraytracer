cf59ba992e5d94e4e837b1bf0b6fde46088ca069
Andy Baird <andy@threadsculture.com>
2022-02-23 13:24:35 -0700

    Test re-org

--- spec/shape_spec.cr
+++ spec/shape_spec.cr
@@ -10,5 +10,46 @@ describe Shape do
         a.should_not eq b
       end
     end
+    describe "intersection" do
+      it "ray intersects a sphere at two points" do
+        r = Ray.new(Point.new(0, 0, -5), Vector.new(0, 0, 1))
+        s = Sphere.new()
+        xs = s.intersect(r)
+        xs.size.should eq 2
+        xs.[0]?.should eq 4.0
+        xs.[1]?.should eq 6.0
+      end
+      it "ray intersects a sphere at a tangent" do
+        r = Ray.new(Point.new(0, 1, -5), Vector.new(0, 0, 1))
+        s = Sphere.new()
+        xs = s.intersect(r)
+        xs.size.should eq 2
+        xs[0]?.should eq 5.0
+        xs[1]?.should eq 5.0
+      end
+      it "ray misses a sphere" do
+        r = Ray.new(Point.new(0, 2, -5), Vector.new(0, 0, 1))
+        s = Sphere.new()
+        xs = s.intersect(r)
+        xs.size.should eq 0
+      end
+      it "ray orginates inside a sphere" do
+        r = Ray.new(Point.new(0, 0, 0), Vector.new(0, 0, 1))
+        s = Sphere.new()
+        xs = s.intersect(r)
+        xs.size.should eq 2
+        xs[0]?.should eq -1.0
+        xs[1]?.should eq 1.0
+      end
+      it "sphere is behind a ray" do
+        r = Ray.new(Point.new(0,0,5), Vector.new(0,0,1))
+        s = Sphere.new()
+        xs = s.intersect(r)
+        xs.size.should eq 2
+        xs[0]?.should eq -6.0
+        xs[1]?.should eq -4.0
+      end
+    end
+    
   end
 end
