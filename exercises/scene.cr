2d72752db654afc3fa56312407cbffe24624420f
Andy Baird <andybaird@gmail.com>
2022-05-20 17:49:48 -0600

    Add some patterns to scene

--- exercises/scene.cr
+++ exercises/scene.cr
@@ -6,6 +6,7 @@ floor.transform = Transform.scale(10, 0.01, 10)
 floor.material = Material.new
 floor.material.color = Color.new(1, 0.9, 0.9)
 floor.material.specular = 0
+floor.material.pattern = StripePattern.new(Color.new(0,0.7,0), Color.black)
 
 left_wall = Sphere.new
 left_wall.transform = Transform.translate(0,0,5)
@@ -28,6 +29,7 @@ middle.material = Material.new
 middle.material.color = Color.new(0.1, 1, 0.5)
 middle.material.diffuse = 0.7
 middle.material.specular = 0.3
+middle.material.pattern = GradientPattern.new(Color.new(0.1,1,0.5), Color.new(0,0,0.7))
 
 right = Sphere.new
 right.transform = Transform.translate(1.5, 0.5, -0.5).scale(0.5, 0.5, 0.5)
@@ -50,7 +52,7 @@ world.light = Lights::Point.new(Point.new(-10, 10, -10), Color.new(1, 1, 1))
   world.objects << object
 end
 
-camera = Camera.new(640, 480, Math::PI/3)
+camera = Camera.new(140, 80, Math::PI/3)
 camera.transform = Transform.view_transform(Point.new(0, 1.5, -5),
                                              Point.new(0, 1, 0),
                                              Vector.new(0, 1, 0))
