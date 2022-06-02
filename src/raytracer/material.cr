b64b111e193c4793a01fff0b23ffd07ca3e443b1
Andy Baird <andybaird@gmail.com>
2022-06-01 20:23:03 -0600

    Making some var names clearer

--- src/raytracer/material.cr
+++ src/raytracer/material.cr
@@ -22,7 +22,7 @@ class Material
     pattern.pattern_at_object(object, point)
   end
 
-  def lighting(light : Lights::Point, object : Shape, point : CTuple, eye_v : CTuple, normal_v : CTuple, in_shadow : Bool = false)
+  def lighting(light : Lights::Point, object : Shape, point : CTuple, eye_v : CTuple, normal_v : CTuple, in_shadow? : Bool = false)
     effective_color = color_at(point, object) * light.intensity
     light_v = (light.position - point).normalize
 
@@ -36,18 +36,18 @@ class Material
       diffuse = effective_color * @diffuse * light_dot_normal
 
       reflect_v = -light_v.reflect(normal_v)
-      reflect_dot_eye = reflect_v.dot(eye_v)
+      reflect_dot_eye_v = reflect_v.dot(eye_v)
 
-      specular = if reflect_dot_eye <= 0
+      specular = if reflect_dot_eye_v <= 0
         Color.black
       else
-        factor = reflect_dot_eye ** @shininess
+        factor = reflect_dot_eye_v ** @shininess
         light.intensity * @specular * factor
       end
     end
     
     v = ambient
-    v += diffuse + specular unless in_shadow
+    v += diffuse + specular unless in_shadow?
 
     Color.new(v.x, v.y, v.z)
   end
